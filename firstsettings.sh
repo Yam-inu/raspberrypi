4d3302b5914e7d1797a845cd7b4fa06718afaa92
Yam-inu <83753313+Yam-inu@users.noreply.github.com>
2024-05-28 11:08:02 +0900

    Create firstsettings.sh

---
+++ firstsettings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#初期設定用シェルスクリプト
+
+# 実行できない場合は以下のコマンドを実行すること
+# sed -i 's/\r//' *.sh
+
+# ユーザディレクトリを英語に変更
+LC_ALL=C xdg-user-dirs-update --force
+cd
+rm -r ダウンロード テンプレート 公開 ドキュメント 音楽 ビデオ 画像
+
+sudo apt update
+
+sudo apt install fcitx-mozc -y
+sudo apt install vim -y
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+setxkbmap jp
